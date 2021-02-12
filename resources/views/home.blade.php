ab64e5f67453dabf6a3177fdafeeabce8fac0e28
Sazid <initialsaremsu@gmail.com>
2021-02-12 17:41:04 +0600

    created a few views and controllers

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="main d-flex flex-row flex-wrap justify-content-around align-items-center align-content-center">
+            <div class="welcome-msg d-inline mb-5">
+                <h1 class="welcome">Welcome to ASW</h1>
+                <h4 class="automate">Automate Your Class with Attendance Scanning</h4>
+            </div>
+            <div class="illustration">
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/about', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
+Route::get('/process', [App\Http\Controllers\AboutController::class, 'index'])->name('process');
